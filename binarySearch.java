54d4f48ce5071e0cb79759c58e855c1e44cd2d9f
Alex Leventer <alexleventer@gmail.com>
2017-07-26 17:36:34 -0700

    Add binarySearch

---
+++ binarySearch.java
@@ -0,0 +1,14 @@
+public static int binarySearch(int[] a, int target) {
+  int left = 0, right = a.length -1;
+  while (left <= right) {
+    int mid = (left + right) / 2;
+
+    if(a[mid] == target)
+      return mid;
+    else if (a[mid] < target)
+      left = mid + 1;
+    else
+      right = mid -1;
+  }
+  return -1;
+}
