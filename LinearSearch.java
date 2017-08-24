1da54afc393aa0bdcac2c25bb5decf8a83b34328
Alex Leventer <alexleventer@gmail.com>
2017-08-23 21:59:43 -0700

    Cleanup linear search

--- LinearSearch.java
+++ LinearSearch.java
@@ -1,8 +1,10 @@
-public static int selectionSort(int[] a, int target) {
-  for (int i = 0; i < a.length; i++) {
-    if(a[i] == target) {
-      return i;
+public class LinearSearch {
+  public static int selectionSort(int[] a, int target) {
+    for (int i = 0; i < a.length; i++) {
+      if (a[i] == target) {
+        return i;
+      }
     }
+    return -1;
   }
-  return -1;
 }
