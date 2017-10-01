293aa2d60fbd7fd5477c9946c14f2a78fb14ae5d
Alex Leventer <alexleventer@gmail.com>
2017-09-30 18:32:20 -0700

    Update RecursiveBinarySearch.java

--- RecursiveBinarySearch.java
+++ RecursiveBinarySearch.java
@@ -1,16 +1,16 @@
 public class RecursiveBinarySearch {
-  int binarySearch(int[] arr, int key) {
+  public static int binarySearch(int[] arr, int key) {
     return binarySearch(arr, 0, arr.length - 1, key);
   }
 
-  int binarySearch(int[] arr, int lo, int hi, int key) {
+  public static int binarySearch(int[] arr, int lo, int hi, int key) {
     if (lo <= hi) {
       int mid = (lo + hi) / 2;
       if (key == arr[mid])
         return mid;
       else if (key < arr[mid])
         return binarySearch(arr, lo, mid - 1, key);
-      else if (key > arr[mid])
+      else
         return binarySearch(arr, mid + 1, hi, key);
     }
     return -1;
